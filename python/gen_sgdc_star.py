b81780816a05263109059b9aeec111a0303efd50
GSXAM <luongcongduchandsome@gmail.com>
2024-06-18 13:35:00 +0700

    Function GEN SGDC completed. Updating function GEN StarECO and in progress diagram for GEN StarECO.

--- python/gen_sgdc_star.py
+++ python/gen_sgdc_star.py
@@ -8,34 +8,29 @@ import openpyxl
 import csv
 import os
 
-def excel_to_csv(excel_file_path, sheet_names, output_folder="."):
+def excel_to_csv(excel_file_path, sheet_name, output_folder="."):
     """
     Reads an Excel file and saves specified sheets as CSV files.
 
     Args:
         excel_file_path (str)   : Path to the Excel file.
-        sheet_names (list)      : List of sheet names to save as CSV.
+        sheet_name (str)        : List of sheet names to save as CSV.
         output_folder (str)     : Path to the output folder where CSV files will be saved.
                                   (default output folder is current directory.)
     Return:
         csv_file_path (str)     : Path to the CSV file.
     """
     wb = openpyxl.load_workbook(excel_file_path, read_only=True, data_only=True)
-
-    for sheet_name in sheet_names:
-        sheet = wb[sheet_name]
-        csv_file_path = f"{output_folder}/{sheet_name}.csv"
-
-        with open(csv_file_path, "w", newline="") as csv_file:
-            csv_writer = csv.writer(csv_file)
-            for row in sheet.iter_rows():
-                csv_writer.writerow([cell.value for cell in row])
-
-        print(f">>>>>>>>>>>> {sheet_name} saved as: {csv_file_path}\n")
-    
+    sheet = wb[sheet_name]
+    csv_file_path = f"{output_folder}/{sheet_name}.csv"
+    with open(csv_file_path, "w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        for row in sheet.iter_rows():
+            csv_writer.writerow([cell.value for cell in row])
+    print(f">>>>>>>>>>>> {sheet_name} saved as: {csv_file_path}\n")
     return csv_file_path
 
-def gen_sgdc(csv_file_path, output_folder, design="DESIGN_NAME"):
+def gen_sgdc(csv_file_path, output_folder=".", design="DESIGN_NAME"):
     """
     Reads a CSV file and generates SGDC file constraint.
 
@@ -48,9 +43,7 @@ def gen_sgdc(csv_file_path, output_folder, design="DESIGN_NAME"):
         sgdc_file_path (str)    : Path to the SGDC file.
     """
     sgdc_file_path = f"{output_folder}/{design}.sgdc"
-    sgdc_writer = open(sgdc_file_path, 'w')
-    print(f'current_design "{design}"\n\n')
-    sgdc_writer.write(f'current_design "{design}"\n\n')
+    writelist = [f'current_design "{design}"\n\n']
 
     with open(csv_file_path, 'r') as csvfile:
         csvload = csv.reader(csvfile, delimiter=',')
@@ -58,34 +51,56 @@ def gen_sgdc(csv_file_path, output_folder, design="DESIGN_NAME"):
         for row in csvload:
             if (row[0].isnumeric()):
                 if (row[2] == "reset"):
-                    cmd = f'reset -async -name "{row[1]}"\n\ttest_mode -scanshift -value {row[3]} -name "{row[1]}"'
-                    print(cmd)
-                    sgdc_writer.write(f"{cmd}\n")
+                    writelist.append(f'reset -async -name "{row[1]}"\n\ttest_mode -scanshift -value {row[3]} -name "{row[1]}"\n')
                 elif (row[2] == "clock"):
                     if (row[3].isnumeric()):
-                        cmd = f'clock -testclock -frequency {row[3]} -name "{row[1]}"'
-                        print(cmd)
-                        sgdc_writer.write(f"{cmd}\n")
+                        writelist.append(f'clock -testclock -frequency {row[3]} -name "{row[1]}"\n')
                     else:
-                        cmd = f'clock -testclock -name "{row[1]}"'
-                        print(cmd)
-                        sgdc_writer.write(f"{cmd}\n")
+                        writelist.append(f'clock -testclock -name "{row[1]}"\n')
                 elif (row[2] == "contraint"):
-                    cmd = f'test_mode -value {row[3]} -name "{row[1]}"'
-                    print(cmd)
-                    sgdc_writer.write(f"{cmd}\n")
+                    writelist.append(f'test_mode -value {row[3]} -name "{row[1]}"\n')
+    
+    sgdc_writer = open(sgdc_file_path, 'w')
+    sgdc_writer.writelines(writelist)
     sgdc_writer.close()
     return sgdc_file_path
 
 
+def gen_starECO(csv_file_path, output_folder=".", design="DESIGN_NAME"):
+    """
+    Reads a CSV file and generates StarECO file commands.
+
+    Args:
+        csv_file_path (str)     : Path to the CSV file.
+        output_folder (str)     : Path to the output folder where SGDC files will be saved.
+        design (str)            : Design name (Ex: "HLB18_MAIN", default is "DESIGN_NAME")
+
+    Return:
+        starECO_file_path (str) : Path to the StarECO file.
+    """
+    starECO_file_path = f"{output_folder}/{design}_star_scr_gen.tcl"
+    writelist = ['##############################\n', \
+                '#         COMMON             #\n', \
+                '##############################\n', \
+                'add_instance SNIDFT_MBIST_OR_SCAN -reference ${ma_stdor}\n', \
+                'add_connection -from SNIDFT_mbist_mode -to SNIDFT_MBIST_OR_SCAN/A1\n', \
+                'add_connection -from SNIDFT_scan_mode -to SNIDFT_MBIST_OR_SCAN/A2\n\n']
+
+    # Add code to processing csv file
+
+    starECO_writer = open(starECO_file_path, 'w')
+    starECO_writer.writelines(writelist)
+    starECO_writer.close()
+    return starECO_file_path
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #                   MAIN.py
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 excel_file_path = "ecolist.xlsx"
-sheet_names_to_save = ["ecolist"]  # Add the desired sheet names
+sheet_names_to_save = "ecolist"  # Add the desired sheet names
 output_folder_path = "."  # Specify the output folder
+design_name = "HLB18_MAIN"
 
 csv_file_path = excel_to_csv(excel_file_path, sheet_names_to_save, output_folder_path)
-sgdc_file_path = gen_sgdc(csv_file_path, output_folder_path, "HLB18_MAIN")
-
+sgdc_file_path = gen_sgdc(csv_file_path, output_folder_path, design_name)
+starECO_file_path = gen_starECO(csv_file_path, output_folder_path, design_name)
--- python/test.py
+++ python/test.py
@@ -1,20 +1,44 @@
 import os
-def check_number(variable):
-    if variable.isnumeric():
-        print(f"{variable} is a number.")
-    else:
-        print(f"{variable} is not a number.")
+# def check_number(variable):
+#     if variable.isnumeric():
+#         print(f"{variable} is a number.")
+#     else:
+#         print(f"{variable} is not a number.")
 
-# Example usage
-check_number("123.45")  # Output: 123.45 is a number.
-check_number("")   # Output: hello is not a number.
+# # Example usage
+# check_number("123.45")  # Output: 123.45 is a number.
+# check_number("")   # Output: hello is not a number.
 
 
 # if (os.path.isfile("ecolist.csv")):
 #     os.remove("ecolist.csv")
 # else:
+writelist = ["1. initilized string\n"]
+
+a = 199
+
+cmd = f"2. second line\n"
+writelist.append(cmd)
+cmd = f"3. third line\n"
+writelist.append(cmd)
+cmd = f"4. fourth line\n"
+writelist.append(cmd)
+cmd = f"5. fiveth line: a={a}\n"
+writelist.append(cmd)
+
+cmd = ['##############################\n', \
+       '#         COMMON             #\n', \
+       '##############################\n', \
+       'add_instance SNIDFT_MBIST_OR_SCAN -reference ${ma_stdor}\n', \
+       'add_connection -from SNIDFT_mbist_mode -to SNIDFT_MBIST_OR_SCAN/A1\n', \
+       'add_connection -from SNIDFT_scan_mode -to SNIDFT_MBIST_OR_SCAN/A2\n']
+writelist.extend(cmd)
+a = 299
+cmd = f"changed. a={a}\n"
+writelist.append(cmd)
+
+
 file = open("data.txt", 'w')
-file.write("hello\n")
-file.write("12345\n")
-file.write("lkhjg\n")
-file.write("zxcvb\n")
+print(writelist)
+file.writelines(writelist)
+file.close()
